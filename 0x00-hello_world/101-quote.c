10afe0ea3096f2f55b33e253bd5a97f74aa0db6f
Mahmoud Malek <information.mahmoud@gmail.com>
2023-07-13 22:48:13 +0000

    NEW

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,";
+
+	write(STDERR_FILENO, msg, sizeof(msg) - 1);
+	return (1);
+}
