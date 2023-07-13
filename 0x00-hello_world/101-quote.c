695fd6608aab498f1a16e65fcdcd5fedc8419dad
Mahmoud Malek <information.mahmoud@gmail.com>
2023-07-13 22:55:57 +0000

    u

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Return: 1 (error)
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,";
-
-	write(1, msg, sizeof(msg) - 1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
