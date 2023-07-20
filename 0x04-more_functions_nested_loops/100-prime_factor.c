0df9e72140ad96cade2eee10bdd1bffa875abd09
Mahmoud Malek <information.mahmoud@gmail.com>
2023-07-20 16:17:11 +0000

    New command

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - find and print largest prime factor
+* Description: largest prime factor
+* Return: always 0 (succss)
+*/
+
+int main(void)
+{
+	long int n = 612852475143, div = 2, maxf;
+
+	while (n > 0)
+	{
+		if (n % div != 0)
+			div++;
+
+		else
+		{
+			maxf = n;
+			n /= div;
+		}
+
+		if (n == 1)
+		{
+			printf("%lu \n", maxf);
+			break;
+		}
+	}
+
+	return (0);
+}
