1eb84013ad4e3eb6c8945bd496d5e114dca9c653
Mahmoud Malek <information.mahmoud@gmail.com>
2023-07-20 18:35:16 +0000

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,17 @@
 
 void print_number(int n)
 {
+	unsigned int n2 = n;
+
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n2 = -n2;
 	}
 
-	if ((n / 10) > 0)
-		print_number((n / 10));
+	if ((n2 / 10) > 0)
+		print_number((n2 / 10));
 
-	_putchar((n % 10) + '0');
+	_putchar((n2 % 10) + '0');
 }
