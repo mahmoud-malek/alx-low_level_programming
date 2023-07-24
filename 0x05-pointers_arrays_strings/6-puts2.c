e91f0c2375d4a273a8707bcb9b6b0e74b4302ea0
Mahmoud Malek <information.mahmoud@gmail.com>
2023-07-24 17:37:28 +0000

    function that prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - reverses a string
+ * @str: string to be reversed
+ */
+
+void puts2(char *str)
+{
+	int i;
+	
+	for (i = 0; *str != '\0' ; i++)
+	{
+		if (i % 2 == 0 )
+			printf("%c", *str);
+		str++;
+	}
+	printf("\n");
+}
