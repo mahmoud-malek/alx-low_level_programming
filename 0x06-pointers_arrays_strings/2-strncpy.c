e565f723941848e3512539ca28fba7c14326b661
Mahmoud Malek <information.mahmoud@gmail.com>
2023-07-26 19:30:39 +0000

    M

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncpy - a function that concatenates two strings.
+ * @dest: destination input
+ * @src: source input
+ * @n: number of bytes to cat
+ * Return: pointer to resulting string dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+	int i = 0;
+
+	while ((i < n) && (src[i] != '\0'))
+	{
+		ptr[i] = src[i];
+		i++;
+	}
+
+	if (src[i] == '\0')
+		ptr[i] = '\0';
+
+	return (dest);
+}
