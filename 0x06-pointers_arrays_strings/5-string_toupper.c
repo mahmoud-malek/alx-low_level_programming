b94e3eb668f09766dea370e53985b707f305f4c6
Mahmoud Malek <information.mahmoud@gmail.com>
2023-07-27 10:44:50 +0000

    convert string to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_touppper - converts string to upper case
+ * @str: array
+ * Return: pointer to resulting string 
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] -= 32;
+		}
+	}
+
+	return (str);	
+}
