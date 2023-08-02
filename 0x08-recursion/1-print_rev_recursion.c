f400ccd335a093e04efbcb15d238fc406834d2d9
Mahmoud Malek <information.mahmoud@gmail.com>
2023-08-02 14:47:01 +0000

    ok

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - a function that prints string recursively
+ * @s: buffer to print
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s += 1);
+	printf("%c", *s);
+
+}
