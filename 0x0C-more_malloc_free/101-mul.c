4be9c5488011eaaafd8670d8525f354ccabb4ec0
mahmoud <information.mahmoud@gmail.com>
2023-08-11 20:13:01 +0300

    yes

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,34 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 #include "main.h"
-
-/**
- * digit - checks if a non-digit char
- * @c: char to be evaluated
- * Return: 0 if a non-digit is found, 1 otherwise
- */
-int digit(char c)
-{
-	if (c < '0' || c > '9')
-		return (0);
-	return (1);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-
-	return (i);
-}
 
 /**
  * valid - checks whether str has non digit
@@ -37,12 +11,12 @@ int _strlen(char *s)
  */
 int valid(char *str)
 {
-	 int i;
+	unsigned int i;
 
 	if (str == NULL)
 		return (0);
-	for (i = 0; i < _strlen(str); i++)
-		if (!digit(str[i]))
+	for (i = 0; i < strlen(str); i++)
+		if (!isdigit(str[i]))
 			return (0);
 
 	return (1);
@@ -59,32 +33,18 @@ void myexit(void)
 }
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
+ * multiply - multiplies two positive numbers
+ * @res: resulting array
+ * @n1: num1
+ * @n2: num2
+ * @len1: length1
+ * @len2: length2
  * Return: always 0 (Success)
  */
 
-int main(int argc, char **argv)
+void multiply(char *res, char *n1, char *n2, int len1, int len2)
 {
-	char *n1, *n2, *res;
-	int len1, len2, len, i, j, num;
-
-	n1 = argv[1], n2 = argv[2];
-	if (argc != 3 || !valid(n1) || !valid(n2))
-		myexit();
-
-	len1 = _strlen(n1);
-	len2 = _strlen(n2);
-	len = len1 + len2 + 1;
-	res = malloc(sizeof(char) * len);
-
-	if (res == NULL)
-		myexit();
-
-	for (i = 0; i <= len1 + len2; i++)
-		res[i] = '0';
+	int i, j, num;
 
 	for (i = len1 - 1; i >= 0; i--)
 	{
@@ -98,8 +58,47 @@ int main(int argc, char **argv)
 		if (num)
 			res[i + j + 1] = num + '0';
 	}
+
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	char *n1, *n2, *res;
+	int len1, len2, len, i;
+
+	n1 = argv[1], n2 = argv[2];
+	if (argc != 3 || !valid(n1) || !valid(n2))
+		myexit();
+
+	len1 = strlen(n1);
+	len2 = strlen(n2);
+	len = len1 + len2 + 1;
+	if (n1[0] == '0' || n2[0] == '0')
+	{
+		putchar('0');
+		putchar('\n');
+	}
+	res = malloc(sizeof(char) * len);
+
+	if (res == NULL)
+		myexit();
+
+	for (i = 0; i <= len1 + len2; i++)
+		res[i] = '0';
+
+	multiply(res, n1, n2, len1, len2);
 	i = 0;
-	while (res[i++] == '0')
+	while (res[i] == '0')
+		i++;
+
 	for (; i < len - 1; i++)
 		putchar(res[i]);
 
