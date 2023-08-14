3d2df59b7a8190f821456c6c0b0ae0fe483aad9f
mahmoud <information.mahmoud@gmail.com>
2023-08-14 21:00:59 +0300

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,10 +18,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	dog->name = malloc(strlen(name) + 1);
-	dog->owner = malloc(strlen(owner) + 1);
 
-	if (dog->name == NULL || dog->owner == NULL)
+	if (dog->name == NULL)
+	{
+		free(dog->name);
 		return (NULL);
+	}
+
+	dog->owner = malloc(strlen(owner) + 1);
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		return (NULL);
+	}
 
 	strcpy(dog->name, name);
 	strcpy(dog->owner, owner);
