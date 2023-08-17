c568dbe456470ccea6cc06069be5c8946d180904
mahmoud <information.mahmoud@gmail.com>
2023-08-17 18:22:12 +0300

    udpate

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -12,16 +12,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char *const format, ...);
 
-/**
- * struct print - struct
- * @str: is the specifier to be printted
- * @f: is a function
- */
-
-typedef struct print
-{
-	char *str;
-	void (*f)(va_list);
-} print;
-
 #endif /*VARIADIC HEADER*/
