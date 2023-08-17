092509297455d3c16d49dd38834d7b6e9ee72b79
mahmoud <information.mahmoud@gmail.com>
2023-08-17 18:20:50 +0300

    finally

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,87 +1,51 @@
 #include "variadic_functions.h"
 
 /**
- * print_int - a function that prints strings, followed by a new line
- * @list: is the specifier to be printted
- */
-
-void print_int(va_list list)
-{
-	printf("%d", va_arg(list, int));
-}
-
-/**
- * print_char - a function that prints strings, followed by a new line
- * @list: is the specifier to be printted
- */
-
-void print_char(va_list list)
-{
-	printf("%c", va_arg(list, int));
-}
-
-/**
- * print_string - a function that prints strings, followed by a new line
- * @list: is the specifier to be printted
- */
-
-void print_string(va_list list)
-{
-	char *v = va_arg(list, char *);
-
-	if (v != NULL)
-	{
-		printf("%s", v);
-		return;
-	}
-
-	printf("(nil)");
-}
-
-/**
- * print_float - a function that prints strings, followed by a new line
- * @list: is the specifier to be printted
- */
-void print_float(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-
-/**
  * print_all - a function that prints strings, followed by a new line
  * @format: is the specifier to be printted
  */
 
 void print_all(const char *const format, ...)
 {
-	int i, j = 0;
+	int i = 0;
 	va_list vals;
 	char *separator = "";
 
-	print pr[] = {
-		{"c", print_char},
-		{"f", print_float},
-		{"i", print_int},
-		{"s", print_string},
-		{NULL, NULL}};
-
 	va_start(vals, format);
 
-	while (format && format[j])
+	while (format && format[i])
 	{
-		i = 0;
-		while (pr[i].str != NULL)
+		printf("%s", separator);
+		switch (format[i])
 		{
-			if (format[j] == pr[i].str[0])
+		case 'c':
+			printf("%c", va_arg(vals, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(vals, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(vals, double));
+			break;
+		case 's':
+		{
+			char *v = va_arg(vals, char *);
+
+			if (v == NULL)
 			{
-				printf("%s", separator);
-				pr[i].f(vals);
-				separator = ", ";
+				v = "(nil)";
 			}
-			i++;
+			printf("%s", v);
+			break;
 		}
-		j++;
+		default:
+			i++;
+			continue;
+		}
+		separator = ", ";
+		i++;
 	}
 	printf("\n");
 	va_end(vals);
+}
 }
