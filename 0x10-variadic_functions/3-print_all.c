451b84d5536db7beb1ab468e4952d6cb7accff08
mahmoud <information.mahmoud@gmail.com>
2023-08-17 18:18:26 +0300

    yes

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,21 +1,87 @@
 #include "variadic_functions.h"
 
 /**
+ * print_int - a function that prints strings, followed by a new line
+ * @list: is the specifier to be printted
+ */
+
+void print_int(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * print_char - a function that prints strings, followed by a new line
+ * @list: is the specifier to be printted
+ */
+
+void print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+ * print_string - a function that prints strings, followed by a new line
+ * @list: is the specifier to be printted
+ */
+
+void print_string(va_list list)
+{
+	char *v = va_arg(list, char *);
+
+	if (v != NULL)
+	{
+		printf("%s", v);
+		return;
+	}
+
+	printf("(nil)");
+}
+
+/**
+ * print_float - a function that prints strings, followed by a new line
+ * @list: is the specifier to be printted
+ */
+void print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
  * print_all - a function that prints strings, followed by a new line
  * @format: is the specifier to be printted
-*/
+ */
 
-void print_all(const char * const format, ...)
+void print_all(const char *const format, ...)
 {
-
-	int len = strlen(format);
+	int i, j = 0;
 	va_list vals;
-	
-	while (len--)
-	{
-		*format
-	}
-	
+	char *separator = "";
 
-printf("\n");
+	print pr[] = {
+		{"c", print_char},
+		{"f", print_float},
+		{"i", print_int},
+		{"s", print_string},
+		{NULL, NULL}};
+
+	va_start(vals, format);
+
+	while (format && format[j])
+	{
+		i = 0;
+		while (pr[i].str != NULL)
+		{
+			if (format[j] == pr[i].str[0])
+			{
+				printf("%s", separator);
+				pr[i].f(vals);
+				separator = ", ";
+			}
+			i++;
+		}
+		j++;
+	}
+	printf("\n");
+	va_end(vals);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,6 +9,19 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+
+void print_all(const char *const format, ...);
+
+/**
+ * struct print - struct
+ * @str: is the specifier to be printted
+ * @f: is a function
+ */
+
+typedef struct print
+{
+	char *str;
+	void (*f)(va_list);
+} print;
 
 #endif /*VARIADIC HEADER*/
