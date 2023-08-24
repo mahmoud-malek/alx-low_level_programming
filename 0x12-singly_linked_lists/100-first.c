b423b6afc429e7900b2dbab929aba75196bca8c4
mahmoud <information.mahmoud@gmail.com>
2023-08-24 23:57:42 +0300

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
 
 void beforee(void)
 {
-	printf("You're beat! and yet,you must allow,");
-	printf("\nI bore my house upon my back!\n");
+	printf("You're beat! and yet,you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
