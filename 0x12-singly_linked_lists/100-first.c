87b587ed52a38dc7ac45d5363e9d8a61cee034c7
mahmoud <information.mahmoud@gmail.com>
2023-08-24 23:40:21 +0300

    beat beat

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * beforee - a function that prints string before main
+ * Description: this function is executed before the main
+ */
+
+void beforee(void)
+{
+	printf("You're beat! and yet,you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
