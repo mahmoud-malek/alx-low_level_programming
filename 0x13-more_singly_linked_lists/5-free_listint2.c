2d9f4fb7ec8b7d171e6d6a2dc4ffa0361fd5f502
mahmoud malek <information.mahmoud@gmail.com>
2023-08-28 19:13:07 +0300

    free 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a listint_t list.
+ * @head: is a strcut contain elements and head node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *hold = *head;
+
+	if (head != NULL)
+	{
+		while (hold != NULL)
+		{
+			*head = (*head)->next;
+			free(hold);
+			hold = *head;
+		}
+	}
+
+	head = NULL;
+}
