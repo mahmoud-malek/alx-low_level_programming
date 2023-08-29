ed50449a157a01e00250fb3741df4f9d9b0d24a1
mahmoud malek <information.mahmoud@gmail.com>
2023-08-29 22:21:39 +0300

    ok

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -23,6 +23,7 @@ size_t print_listint_safe(const listint_t *head)
 		if (current <= current->next)
 		{
 			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+			nodes++;
 			return (nodes);
 		}
 		current = current->next;
