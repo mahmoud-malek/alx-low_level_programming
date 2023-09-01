e710f6cebb53a70b8a3605be327c1fec0e6566c0
mahmoud malek <information.mahmoud@gmail.com>
2023-09-01 14:57:43 +0300

    practice

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * print_binary2 -a function that prints the binary
+ *  representation of a number.
+ * @n: number
+ * Return: the converted number, 0 otherwise
+ */
+
+void print_binary2(unsigned long int n)
+{
+	if (n > 1)
+		print_binary2(n >> 1);
+	putchar((n & 1) ? '1' : '0');
+}
+
+/**
  * print_binary -a function that prints the binary
  *  representation of a number.
  * @n: number
@@ -9,7 +23,19 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	putchar((n & 1) ? '1' : '0');
+	int bits_size = sizeof(n) * 8;
+	int one = 0;
+
+	while (bits_size)
+	{
+		bits_size--;
+		if (n & (1UL << bits_size))
+			putchar('1'), one = 1;
+
+		else if (one)
+			putchar('0');
+	}
+
+	if (!one)
+		putchar('0');
 }
