4d0cfcd9af08508627661d0a1bb35944c6b5efb1
mahmoud <information.mahmoud@gmail.com>
2023-11-02 17:32:27 +0200

    ok

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,9 +9,25 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long i = 0;
 
-	table = malloc(sizeof(hash_node_t) * size);
+	if (!size)
+		return (NULL);
+
+	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (table);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+	if (!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	for (; i < size; i++)
+		table->array[i] = NULL;
+
 	return (table);
 }
