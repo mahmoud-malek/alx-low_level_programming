ddbc1f4b95712c0b3216f7091fb9273478b28a51
mahmoud malek <information.mahmoud@gmail.com>
2024-04-11 09:51:09 +0200

    ok

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,6 +1,42 @@
 #include "search_algos.h"
 
 /**
+ * lower_bound - a function that searches for a value of
+ * sorted array of integers
+ * @array: is the set of integers
+ * @top: is the top of the array
+ * @down: is the bottom of the array
+ * @value: is the target to search for
+ * Return: index of the target value, -1 otherwise
+ */
+
+int lower_bound(int *array, int top, int down, int value)
+{
+	int mid = (top + down) / 2;
+	int i;
+
+	if (top == down && array[mid] != value)
+		return (-1);
+
+	printf("Searching in array:");
+	for (i = down; i <= top; i++)
+	{
+		printf(" %d", array[i]);
+		if (i < top)
+			printf(",");
+	}
+	printf("\n");
+
+	if (top == down)
+		return (mid);
+
+	if (array[mid] < value)
+		return (lower_bound(array, top, mid + 1, value));
+
+	return (lower_bound(array, mid, down, value));
+}
+
+/**
  * advanced_binary - a function that searches for a value of
  * sorted array of integers
  * @array: is the set of integers
@@ -11,35 +47,8 @@
 
 int advanced_binary(int *array, size_t size, int value)
 {
-	int mid, top, down, i;
-
 	if (!array)
 		return (-1);
 
-	top = size - 1;
-	down = 0;
-	while (down <= top)
-	{
-		mid = (top + down) / 2;
-
-		printf("Searching in array:");
-		for (i = down; i <= top; i++)
-		{
-			printf(" %d", array[i]);
-			if (i < top)
-				printf(",");
-		}
-		printf("\n");
-
-		if (array[mid] < value)
-			down = mid + 1;
-
-		else
-			top = mid - 1;
-	}
-
-	if (array[mid] == value)
-		return (mid);
-
-	return (-1);
+	return (lower_bound(array, size - 1, 0, value));
 }
